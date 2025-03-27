3fe4ba17c27fa5e8a4a58395f0b829bfde2c57f8
Haoran987 <zhouhr2828@gmail.com>
2025-03-26 20:34:28 -0700

    jiggless

--- Friends/Assets/Car.cs
+++ Friends/Assets/Car.cs
@@ -2,6 +2,10 @@ using UnityEngine;
 
 public class Car : MonoBehaviour
 {
+
+    public int speed = 40;
+    public int rotationSpeed = 40;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -11,6 +15,15 @@ public class Car : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        float translation = Input.GetAxis("Vertical") * speed;
+        float rotation = Input.GetAxis("Horizontal") * rotationSpeed;
         
+        translation *= Time.deltaTime;
+        rotation *= Time.deltaTime;
+        
+        transform.Translate(0,0, translation);
+        transform.Rotate(0, rotation, 0);
+
+
     }
 }
